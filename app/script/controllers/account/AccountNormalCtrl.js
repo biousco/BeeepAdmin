852b6faa3bc019f1d168cf45bc0b483a4fc16a6f
LinLinML <441936245@qq.com>
2016-03-23 21:35:09 +0800

    账号管理

--- app/script/controllers/account/AccountNormalCtrl.js
+++ app/script/controllers/account/AccountNormalCtrl.js
@@ -3,9 +3,43 @@
  */
 define(['./../module'], function (controllers) {
   'use strict';
-  controllers.controller('AccountNormalCtrl', ['$scope','AdminService',function ($scope, AdminService) {
+  controllers.controller('AccountNormalCtrl', ['$scope','AdminService','$uibModal',function ($scope, AdminService, $uibModal) {
 
+    /** 查看详情 **/
+    $scope.showDetail = function () {
+      var modalInstance = $uibModal.open({
+        templateUrl: 'views/common/modal-simple.html',
+        controller: 'SimpleDialogInstanceCtrl',
+        resolve: {
+          modal : {
+            title: "确认封号吗"
+          }
+        }
+      });
 
+      modalInstance.result.then(function (isBan) {
+        if(!isBan) return false;
+        console.log('yes');
+      }, function () {});
+    };
+
+    /** 封号 **/
+    $scope.banAccount = function () {
+      var modalInstance = $uibModal.open({
+        templateUrl: 'views/common/modal-simple.html',
+        controller: 'SimpleDialogInstanceCtrl',
+        resolve: {
+          modal : {
+            title: "确认封号吗"
+          }
+        }
+      });
+
+      modalInstance.result.then(function (isBan) {
+        if(!isBan) return false;
+        console.log('yes');
+      }, function () {});
+    };
 
   }]);
 });
