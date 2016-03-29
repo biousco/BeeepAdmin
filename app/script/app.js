5a10f6860d8e86f048eb3d8bba5673561c05e8a8
LinLinML <441936245@qq.com>
2016-03-29 17:32:19 +0800

    编辑器

--- app/script/app.js
+++ app/script/app.js
@@ -5,15 +5,20 @@ define([
     'uiBootstrap',
     'uitlps',
     'xeditable',
+    'textAngular',
+    'saveSelection',
     './controllers/index',
     './directives/index',
     './filters/index',
     './services/index'
 ], function (ng) {
+    window.rangy = require('rangy');
+    require('textAngular');
     return ng.module('beepAdminApp', [
       'ui.router',
       'ui.bootstrap',
       'xeditable',
+      'textAngular',
         'app.services',
         'app.controllers',
         'app.filters',
--- app/script/main.js
+++ app/script/main.js
@@ -1,12 +1,20 @@
 require.config({
   baseUrl: './script',
   paths: {
-    angular: '../vender/angular/angular.min',
+    angular: '../vender/angular/angular',
     domReady: '../vender/domready/ready',
     uiRouter: '../vender/angular-ui-router/release/angular-ui-router.min',
     uiBootstrap: '../vender/angular-bootstrap/ui-bootstrap.min',
     uitlps: '../vender/angular-bootstrap/ui-bootstrap-tpls.min',
-    xeditable: '../vender/angular-xeditable/dist/js/xeditable'
+    xeditable: '../vender/angular-xeditable/dist/js/xeditable',
+
+
+
+    rangy: '../vender/rangy/rangy-core',
+    saveSelection: '../vender/rangy/rangy-selectionsaverestore',
+    sanitize: '../vender/textAngular/dist/textAngular-sanitize.min',
+    textAngularSetup: '../vender/textAngular/dist/textAngularSetup',
+    textAngular: '../vender/textAngular/dist/textAngular'
   },
   shim: {
     angular: {
@@ -26,6 +34,29 @@ require.config({
     xeditable: {
       deps: ['angular'],
       exports: 'xeditable'
+    },
+    rangy: {
+      exports: 'rangy'
+    },
+    saveSelection: {
+      deps: ['rangy'],
+      exports: ['saveSelection']
+    },
+    sanitize: {
+      deps: ['angular'],
+      exports: 'sanitize'
+    },
+    textAngularSetup: {
+      deps: ['angular'],
+      exports: 'textAngularSetup'
+    },
+    // saveSelection: {
+    //   deps: ['rangy'],
+    //   exports: 'saveSelection'
+    // },
+    textAngular: {
+      deps: ['angular','sanitize','textAngularSetup'],
+      exports: 'textAngular'
     }
   },
   deps: ['./bootstrap'],
